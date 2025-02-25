249d00ad3833c500c1f4aa7ab96637df5d3dd8c3
Ayumi <ayumi.xie5@gmail.com>
2025-02-25 09:05:20 +0700

    Register
    Halaman register sudah termasuk bootstrap, masih pakai alert, nanti diubah

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,6 +3,10 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
 router.get("/login", (req, res) => {
     res.render("login");
 });
